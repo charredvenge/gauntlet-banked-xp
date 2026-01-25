9c7653e314edeb428c521ec00fab7a22d889818d
Maxwell Chow <maxchowla@gmail.com>
2026-01-24 20:43:59 -0800

    update item id checking and add quantity

--- src/main/java/com/github/charredgrass/gauntletbankedxp/GauntletBankedXpCalculate.java
+++ src/main/java/com/github/charredgrass/gauntletbankedxp/GauntletBankedXpCalculate.java
@@ -1,31 +1,31 @@
 package com.github.charredgrass.gauntletbankedxp;
 
+import net.runelite.api.gameval.ItemID;
 import net.runelite.client.game.ItemStack;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Collection;
 
 public class GauntletBankedXpCalculate {
-    class Items {
-        static final int UNCUT_SAPPHIRE = 1623;
-        static final int UNCUT_EMERALD = 1621;
-        static final int UNCUT_RUBY = 1619;
-        static final int BATTLESTAFF = 1391;
-    }
+    private static final Logger log = LoggerFactory.getLogger(GauntletBankedXpCalculate.class);
+
     public static double calculateLoot(Collection<ItemStack> items) {
         double xp = 0; //crafting xp. probably should intify this?
         for (ItemStack i : items) {
-            switch (i.getId()) {
-                case Items.UNCUT_SAPPHIRE:
-                    xp += 50;
+            log.debug("currid: {}", i.getId());
+            switch (i.getId() - 1) { //dont ask idk lol
+                case ItemID.UNCUT_SAPPHIRE:
+                    xp += 50 * i.getQuantity();
                     break;
-                case Items.UNCUT_EMERALD:
-                    xp += 67.5;
+                case ItemID.UNCUT_EMERALD:
+                    xp += 67.5 * i.getQuantity();
                     break;
-                case Items.UNCUT_RUBY:
-                    xp += 85;
+                case ItemID.UNCUT_RUBY:
+                    xp += 85 * i.getQuantity();
                     break;
-                case Items.BATTLESTAFF:
-                    xp += 100;
+                case ItemID.BATTLESTAFF:
+                    xp += 100 * i.getQuantity();
                     //TODO change based on configured bstaff method
                     break;
             }
